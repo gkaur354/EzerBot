96d1cf5c31579936b8b076d979103a0196835a7b
gkaur354 <82777677+gkaur354@users.noreply.github.com>
2021-08-06 15:24:18 -0400

    Add files via upload

---
+++ basefProject/chatbot.py
@@ -0,0 +1,77 @@
+#Chatbot application that uses the trained MODELLLLL
+
+#Import libraries
+import discord
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
+import pickle
+from tensorflow.keras.models import load_model 
+from nltk.tokenize import word_tokenize
+import re
+from string import punctuation
+
+#Bot login 
+client = discord.Client()
+token = "ODcyOTE5MDc1OTM0ODMwNTkz.YQw3PQ.chMozVGpCcxhK8NHKGlLxhpCkWI"
+
+#Load model and tokenizer
+model = load_model('chatbotmodel.h5')
+
+with open('tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
+
+#Function that removes stopwords
+with open("stopwords_pickle", "rb") as f:
+    stopwords_list = pickle.load(f)
+def remove_stopwords(phrase):
+    noStopwords = ""
+    tokenized_phrase = word_tokenize(phrase)
+    for word in tokenized_phrase:
+        if word not in stopwords_list:
+            noStopwords += word
+            noStopwords += " "
+    return noStopwords
+
+#Function that removes punctuation and numbers
+def clean_text(phrase):
+    pattern = r'[0-9]'
+    final = ""
+    for i in phrase:
+        if i not in punctuation:
+            new_word = re.sub(pattern, '', i)
+            final += new_word
+    return final 
+
+#Function that classifies message
+def classifyMessage(input):
+    cleaned_phrase = clean_text(input.lower())
+    noStopWords = remove_stopwords(cleaned_phrase)
+    
+    phrase = tokenizer.texts_to_sequences([noStopWords.lower()])
+    padded = pad_sequences(phrase, maxlen=50, truncating='post', padding='post')
+    pred = model.predict(padded)
+    pred_int = pred.round().astype("int")
+    if pred_int == 1:
+        msg = "1"
+    else:
+        msg = "0"
+    return msg
+
+#Connect to Discord bot 
+while True:
+    @client.event
+    async def on_ready():
+        print("Ready")
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return 
+        else:
+            res = classifyMessage(message.content)
+            if res != "":
+                await message.reply(res)
+
+            
+    client.run(token)
+
+
---
+++ basefProject/main.py
@@ -0,0 +1,142 @@
+#Description: This model classifies phrases as 1 (indicative of emotional distress) or 0 (other)
+
+#Import libraries
+import sklearn
+import numpy as np
+import nltk
+import tensorflow as tf
+import pandas as pd
+from nltk.corpus import stopwords
+import scipy
+import pickle
+
+from tensorflow.keras.layers import Dense, LSTM, Embedding
+from tensorflow.keras.models import Model
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.initializers import Constant
+from tensorflow.keras.optimizers import Adam
+
+#load the data 
+data = pd.read_csv("/Users/gurnirmalkaur/Desktop/Data.csv")
+
+#List of stopwords
+stop_words = stopwords.words('english')
+
+#Remove I, me, myself, and my from stop words because they provide important information
+last_index = len(stop_words) - 1
+stopWords = stop_words[4:last_index]
+
+#Save list of stopwords
+with open('stopwords_pickle', 'wb') as f:
+    pickle.dump(stopWords,f)
+
+#Data preprocessing:
+
+#Convert to lower case
+data['phrase'] = data['phrase'].apply(lambda x: x.lower())
+
+#Remove punctuation and numbers 
+def only_letters(text):
+    import re
+    pattern = r'[0-9]'
+    from string import punctuation
+    final = ""
+    for i in text:
+        if i not in punctuation:
+            new_word = re.sub(pattern, '', i)
+            final += new_word
+    return final
+
+data['phrase'] = data['phrase'].apply(lambda x:only_letters(x))
+
+#Remove stopwords from text
+def remove_stopwords(text):
+    phrase = nltk.word_tokenize(text)
+    noStopwords = ""
+    for i in phrase:
+        if i not in stopWords:
+            noStopwords += i
+            noStopwords += " "
+    return noStopwords
+
+data['phrase'] = data['phrase'].apply(lambda x:remove_stopwords(x))
+
+#Create corpus 
+from nltk.tokenize import word_tokenize
+def create_corpus(data):
+    corpus = []
+    for phrase in data['phrase']:
+        tokenized_phrase = word_tokenize(phrase)
+        words = []
+        for word in tokenized_phrase:
+            words.append(word)
+        corpus.append(words)
+    return corpus
+
+corpus = create_corpus(data)
+
+#Max phrase length
+maximum = 50
+
+#Spit data into train and test 
+X = data['phrase']
+y = data['intention']
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X,y, test_size = 0.2, random_state = 0)
+
+#Covert to integer array
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
+
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(X_train)
+
+#Save tokenizer
+with open('tokenizer.pickle', 'wb') as handle:
+    pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+train_sequences = tokenizer.texts_to_sequences(X_train)
+
+#Pad train sequences 
+padded_train = pad_sequences(train_sequences, maxlen=maximum, truncating='post', padding='post')
+
+#Pad test sequences
+test_sequences = tokenizer.texts_to_sequences(X_test)
+padded_test = pad_sequences(test_sequences, maxlen=maximum, truncating='post', padding='post')
+
+word_index = tokenizer.word_index
+
+#Create embedding dictionary
+glove_vectors = dict()
+with open("/Users/gurnirmalkaur/Desktop/glove.6B.100d.txt", "r") as f:
+    for line in f:
+        values = line.split()
+        word = values[0]
+        vectors = np.asarray(values[1:])
+        glove_vectors[word] = vectors
+
+#Get vectors for words in data
+num_words = len(word_index) + 1
+embedding_matrix = np.zeros((num_words, 100))
+
+for word, i in word_index.items():
+    if i < num_words:
+        emb_vec = glove_vectors.get(word)
+        if emb_vec is not None:
+            embedding_matrix[i] = emb_vec
+
+#Create model with GloVe
+model = Sequential()
+model.add(Embedding(num_words,100,embeddings_initializer=Constant(embedding_matrix), input_length=maximum, trainable=False))
+model.add(LSTM(100, dropout=0.1))
+model.add(Dense(1, activation="sigmoid"))
+optimizer = Adam(learning_rate=3e-4)
+
+model.compile(loss="binary_crossentropy", optimizer=optimizer, metrics=['accuracy'])
+
+from keras import callbacks
+earlystopping = callbacks.EarlyStopping(monitor="val_loss",mode="min",patience=5, restore_best_weights=True)
+
+hist = model.fit(padded_train,Y_train, epochs=90, validation_data=(padded_test, Y_test),callbacks =[earlystopping],verbose=1)
+model.save('chatbotmodel.h5', hist)
+
