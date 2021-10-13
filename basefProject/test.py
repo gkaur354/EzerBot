5744659072f9717082398aba172e91a26cb13593
Gurnirmal <kaurgurn864@gmail.com>
2021-10-13 18:26:24 -0400

    Update test.py

--- basefProject/test.py
+++ basefProject/test.py
@@ -1,22 +1,30 @@
 #Find resources in the user's vicinity using Yelp API
-
 import requests 
+
 location = "Brantford, Ontario"
-term = "mall"
+category = "c_and_mh"
+term = "mental health"
 url = 'https://api.yelp.com/v3/businesses/search'
 key = open(r'/Users/gurnirmalkaur/Desktop/key.txt').readlines()[0]
-headers = {
+
+def find_resource(location):
+    resources = ""
+    headers = {
     'Authorization': 'Bearer %s' % key
-}
-parameters = {'location': location,
-              'term': term,
-              'radius': 10000,
-              'limit': 10}
+    }       
+    parameters = {'location': location,
+                'categories': category,
+                'term': term,
+                'limit': 3}
+    response = requests.get(url, headers=headers, params=parameters)
+    resource = response.json()['businesses']
 
-response = requests.get(url, headers=headers, params=parameters)
-resource = response.json()['businesses']
+    for r in resource:
+        name = r['name']
+        location = r['location']['address1']
+        city = r['location']['city']
+        province = r['location']['state']
+        resource = (f'{name}, {location}, {city}, {province}\n')
+        resources += resource 
+    return resources
 
-for r in resource:
-    name = r['name']
-    location = r['location']['address1']
-    print(f'{name}, {location}')
