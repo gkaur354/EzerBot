8c1a9b73ea939f4e7117be15ee99949e3ec360d0
Gurnirmal <kaurgurn864@gmail.com>
2022-02-02 22:04:27 -0500

    Update test.py

--- basefProject/test.py
+++ basefProject/test.py
@@ -15,7 +15,7 @@ from sklearn.metrics import f1_score
 
 
 #Load test set 
-data = pd.read_csv("/Users/gurnirmalkaur/Desktop/pathBASEF/testingsetBASEF.csv")
+data = pd.read_csv("/Users/gurnirmalkaur/Desktop/pathBASEF/basefTESTING.csv")
 
 model = load_model('chatbotmodel.h5')
 with open('tokenizer.pickle', 'rb') as handle:
