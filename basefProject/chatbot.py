ed8404dc6bcdfca37f72236350c9c945538b7d73
gkaur354 <82777677+gkaur354@users.noreply.github.com>
2023-07-18 11:25:37 -0400

    Update chatbot.py

--- basefProject/chatbot.py
+++ basefProject/chatbot.py
@@ -1,4 +1,4 @@
-#This file is currently being updated to improve chatbot response intelligence and remove hard-coded reponses. 
+#This file is currently being updated to improve chatbot intelligence and remove hard-coded reponses. 
 
 #Import libraries
 import discord
